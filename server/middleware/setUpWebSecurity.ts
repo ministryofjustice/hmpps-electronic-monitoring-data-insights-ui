97edc94edec394e4a48be4ae3af12aebf7a3ae8e
shahhussaindigitaljustice <shah.Hussain@digital.justice.gov.uk>
2025-03-05 12:30:25 +0000

    Example of integrating an iframe onto nunjucks

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -15,22 +15,24 @@ export default function setUpWebSecurity(): Router {
   })
   router.use(
     helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: ["'self'"],
-          // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
-          // <script nonce="{{ cspNonce }}">
-          // or
-          // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
-          // This ensures only scripts we trust are loaded, and not anything injected into the
-          // page by an attacker.
-          scriptSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
-          styleSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
-          fontSrc: ["'self'"],
-          formAction: [`'self' ${config.apis.hmppsAuth.externalUrl}`],
-        },
-      },
-      crossOriginEmbedderPolicy: true,
+      // TODO - Investigate into CSP
+      contentSecurityPolicy: false,
+      // contentSecurityPolicy: {
+      //   directives: {
+      //     defaultSrc: ["'self'"],
+      //     // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
+      //     // <script nonce="{{ cspNonce }}">
+      //     // or
+      //     // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
+      //     // This ensures only scripts we trust are loaded, and not anything injected into the
+      //     // page by an attacker.
+      //     scriptSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
+      //     styleSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
+      //     fontSrc: ["'self'"],
+      //     formAction: [`'self' ${config.apis.hmppsAuth.externalUrl}`],
+      //   },
+      // },
+      // crossOriginEmbedderPolicy: true,
     }),
   )
   return router
