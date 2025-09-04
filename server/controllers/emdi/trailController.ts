dbca566333c58b96ae3474820ec17a717abb9d95
John Asafo <John.Asafo@justice.gov.uk>
2025-09-04 16:46:09 +0100

    WIP: config change

--- server/controllers/emdi/trailController.ts
+++ server/controllers/emdi/trailController.ts
@@ -1,23 +1,22 @@
-import { NextFunction, RequestHandler } from "express";
-import { TrailPresenter } from "../../presenters/TrailPresenter";
-import { filterByDate, getTrailGeoJson } from "../../services/trailServices";
+import { NextFunction, Request, Response } from 'express'
+import { TrailPresenter } from '../../presenters/TrailPresenter'
+import { filterByDate, getTrailGeoJson } from '../../services/trailServices'
 
-
-type TrailQuery = {from?: string, to?: string}
+type TrailQuery = { from?: string; to?: string }
 export default async function get(
-    req: RequestHandler<unknown, unknown, unknown, TrailQuery>, 
-    res: Response, 
-    next: NextFunction
-)  {
-    try {
-        const filters = {
-            from: (req.query.from as string) || undefined,
-            to: (req.query.to as string) || undefined,
-        };
-        const allGeo = await getTrailGeoJson();
-        const filtered = filterByDate(allGeo, filters);
-        res.render('trail/index', new TrailPresenter(filtered, filters).view());
-    } catch (e) { 
-        next(e); 
+  req: Request<unknown, unknown, unknown, TrailQuery>,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const filters = {
+      from: (req.query.from as string) || undefined,
+      to: (req.query.to as string) || undefined,
     }
+    const allGeo = await getTrailGeoJson()
+    const filtered = filterByDate(allGeo, filters)
+    res.render('trail/index', new TrailPresenter(filtered, filters).view())
+  } catch (e) {
+    next(e)
+  }
 }
