e8f4be2b946b038d02f89ee83d4f6643aa4e446a
John Asafo <John.Asafo@justice.gov.uk>
2025-09-18 08:49:40 +0100

    EPT-415: removed placeholder service

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,14 +7,13 @@ import populateSessionData from '../middleware/populateSessionData'
 import SearchController from '../controllers/search/searchController'
 import CasesController from '../controllers/cases/casesController'
 
-export default function routes({ auditService, emdiService }: Services): Router {
+export default function routes({ auditService }: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
     await auditService.logPageView(Page.HOME_PAGE, { who: res.locals.user.username, correlationId: req.id })
-    const currentTime = await emdiService.getCurrentTime()
-    res.render('pages/index', { activeNav: '/', currentTime })
+    res.render('pages/index', { activeNav: '/' })
   })
 
   const casesController = new CasesController(auditService)
