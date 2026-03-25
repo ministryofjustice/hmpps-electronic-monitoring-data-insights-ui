00f2d72b994ebfcb30e905bf5cbf54879a8558e5
Johny Farrar <me@johnyfarrar.co.uk>
2026-03-25 13:53:21 +0000

    chore: create wiremock stubs

--- integration_tests/mockApis/emdiApi.ts
+++ integration_tests/mockApis/emdiApi.ts
@@ -6,7 +6,7 @@ export default {
     stubFor({
       request: {
         method: 'GET',
-        urlPattern: '/emdi/health/ping',
+        urlPattern: '/health/ping',
       },
       response: {
         status: httpStatus,
@@ -18,12 +18,84 @@ export default {
     stubFor({
       request: {
         method: 'GET',
-        urlPattern: '/emdi/example/time',
+        urlPattern: '/example/time',
       },
       response: {
         status: httpStatus,
         headers: { 'Content-Type': 'application/text;charset=UTF-8' },
         body: '2025-01-01T12:00:00Z',
+      },
+    }),
+  stubPeopleSearchEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people.*',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { persons: [] },
+      },
+    }),
+  stubPersonEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people/[^/]+$',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
+      },
+    }),
+  stubLocationsEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people/[^/]+/locations$',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { locations: [] },
+      },
+    }),
+  stubLocationEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people/[^/]+/locations/[^/]+$',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: [],
+      },
+    }),
+  stubViolationsEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people/[^/]+/curfew/violations$',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { violations: [] },
+      },
+    }),
+  stubViolationEmpty: (): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/people/[^/]+/curfew/violations/[^/]+$',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {},
       },
     }),
   stubProbationPing: (httpStatus = 200): SuperAgentRequest =>
