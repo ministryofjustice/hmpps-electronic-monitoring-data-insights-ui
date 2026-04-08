872d1d570401c09cf3453b90aa201e526ae387a6
John Asafo <John.Asafo@justice.gov.uk>
2026-04-08 15:41:44 +0100

    no fill circle layer

--- assets/js/views/cases/index.ts
+++ assets/js/views/cases/index.ts
@@ -52,7 +52,7 @@ const initialiseLocationDataView = () => {
         positions,
         zIndex: 4,
         style: {
-          fill: 'rgba(0, 0, 0, 0)',
+          fill: 'rgba(76, 128, 182, 1)',
           stroke: {
             color: 'rgba(76, 128, 182, 1)',
           },
@@ -77,7 +77,7 @@ const initialiseLocationDataView = () => {
         visible: true,
         zIndex: 3,
         style: {
-          fill: 'rgba(76, 128, 182, 0.1)',
+          fill: 'rgba(0, 0, 0, 0)',
           stroke: {
             color: 'rgba(76, 128, 182, 1)',
             width: 2,
