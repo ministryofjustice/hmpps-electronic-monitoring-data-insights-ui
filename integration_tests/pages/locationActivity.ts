f2cc14172c2961f7fc049feefe7ba33a1341b0fa
JohnAsafo1 <John.Asafo@justice.gov.uk>
2026-07-08 14:14:55 +0100

    Auto end date populate (#155)

--- integration_tests/pages/locationActivity.ts
+++ integration_tests/pages/locationActivity.ts
@@ -50,25 +50,25 @@ export default class LocationActivityPage extends Page {
     endMinute?: string
   }): void => {
     if (crn) {
-      this.crnInput().type(crn)
+      this.crnInput().clear().type(crn)
     }
     if (startDate) {
-      this.startDateInput().type(startDate)
+      this.startDateInput().clear().type(startDate)
     }
     if (startHour) {
-      this.startHourInput().type(startHour)
+      this.startHourInput().clear().type(startHour)
     }
     if (startMinute) {
-      this.startMinuteInput().type(startMinute)
+      this.startMinuteInput().clear().type(startMinute)
     }
     if (endDate) {
-      this.endDateInput().type(endDate)
+      this.endDateInput().clear().type(endDate)
     }
     if (endHour) {
-      this.endHourInput().type(endHour)
+      this.endHourInput().clear().type(endHour)
     }
     if (endMinute) {
-      this.endMinuteInput().type(endMinute)
+      this.endMinuteInput().clear().type(endMinute)
     }
   }
 
